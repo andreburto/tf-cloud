b4f827760808773e30a2a532de17acba3f9dbf1f
andreburto <tuglyraisin@gmail.com>
2024-01-14 18:56:28 -0600

    Do some aws stuff.

--- main.tf
+++ main.tf
@@ -2,17 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
-
-  # backend "s3" {
-  #   encrypt = true
-  #   bucket = "mothersect-tf-state"
-  #   dynamodb_table = "mothersect-tf-state-lock"
-  #   key    = "tf-cloud"
-  #   region = "us-east-1"
-  # }
 
   cloud {
     organization = "andreburto"
@@ -23,36 +15,35 @@ terraform {
   }
 }
 
-variable "avenger" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
-# # Configure the AWS Provider
-# provider "aws" {
-#   region = "us-east-1"
-# }
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
 
-# locals {
-#   domain_url = "tf-cloid-mothersect.info"
-# }
+locals {
+  domain_url = "tf-cloid-mothersect.info"
+}
 
-# resource "aws_s3_bucket" "tf_cloud" {
-#   bucket = local.domain_url
-# }
+resource "aws_s3_bucket" "tf_cloud" {
+  bucket = local.domain_url
+}
 
-# resource "aws_s3_bucket_ownership_controls" "tf_cloud" {
-#   bucket = aws_s3_bucket.tf_cloud.id
+resource "aws_s3_bucket_ownership_controls" "tf_cloud" {
+  bucket = aws_s3_bucket.tf_cloud.id
 
-#   rule {
-#     object_ownership = "BucketOwnerEnforced"
-#   }
-# }
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
 
-# resource "aws_s3_bucket_acl" "tf_cloud" {
-#   depends_on = [ aws_s3_bucket_ownership_controls.tf_cloud ]
+resource "aws_s3_bucket_acl" "tf_cloud" {
+  depends_on = [ aws_s3_bucket_ownership_controls.tf_cloud ]
 
-#   bucket = aws_s3_bucket.tf_cloud.id
-#   acl = "private"
-# }
-
-output "avenger_name" {
-  value = var.avenger
+  bucket = aws_s3_bucket.tf_cloud.id
+  acl = "private"
 }
